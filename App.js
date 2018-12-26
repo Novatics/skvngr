28b39283653e48aabccaa42b0360959ddcc2bc96
Yurick <yurick.hauschild@gmail.com>
2018-12-26 14:57:23 -0200

    ♻️  Remove more unused code

--- App.js
+++ App.js
@@ -2,21 +2,13 @@ import React, { Component } from "react";
 import { ViroARSceneNavigator } from "react-viro";
 import InitialARScene from "./js/HelloWorldSceneAR";
 
-const sharedProps = {
-  apiKey: "E658630C-44FE-45B4-A235-5031C4930327"
-};
+const API_KEY = "E658630C-44FE-45B4-A235-5031C4930327";
 
-export default class Skvngr extends Component {
-  state = {
-    sharedProps,
-  }
-
-  render() {
+export default function Skvngr() {
     return (
       <ViroARSceneNavigator
-        {...this.state.sharedProps}
+        apiKey={API_KEY}
         initialScene={{ scene: InitialARScene }}
       />
-    );
-  }
+    )
 }
