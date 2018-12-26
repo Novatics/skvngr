047d73b9bbbef3fa8a8a78b1f4172baf6ebd0fe3
Yurick <yurick.hauschild@gmail.com>
2018-12-26 16:58:33 -0200

    ✨  Render novatics logo

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import {
   ViroARScene,
-  ViroMaterials,
   ViroAnimations,
   Viro3DObject,
   ViroARImageMarker,
@@ -18,42 +17,16 @@ ViroARTrackingTargets.createTargets({
 });
 
 export default class HelloWorldSceneAR extends Component {
-  state = {
-    animateCar: false
-  };
-
-  log = () => {
-    console.log("HELLO", "💩 ".repeat(100));
-  };
-
-  _onAnchorFound = () => {
-    this.log();
-    this.setState({
-      animateCar: true
-    });
-  };
-  _toggleButtons = () => {
-    this.setState({
-      animName: this.state.animName == "scaleUp" ? "scaleDown" : "scaleUp"
-    });
-  };
-
   render() {
     return (
       <ViroARScene>
-        <ViroARImageMarker
-          target="qrcode"
-          onAnchorFound={this._onAnchorFound}
-          pauseUpdates={this.state.pauseUpdates}
-        >
+        <ViroARImageMarker target="qrcode">
           <Viro3DObject
-            scale={[0, 0, 0]}
-            source={require("./res/tesla/object_car.obj")}
-            resources={[require("./res/tesla/object_car.mtl")]}
+            rotation={[-90, 0, 0]}
+            scale={[0.001, 0.001, 0.001]}
+            source={require("./res/brand.obj")}
+            resources={[require("./res/brand.mtl")]}
             type="OBJ"
-            materials="white"
-            onClick={this._toggleButtons}
-            animation={{ name: "scaleCar", run: this.state.animateCar }}
           />
 
           <ViroSpotLight
@@ -74,36 +47,10 @@ export default class HelloWorldSceneAR extends Component {
   }
 }
 
-ViroMaterials.createMaterials({
-  white: {
-    lightingModel: "PBR",
-    diffuseTexture: require("./res/tesla/object_car_main_Base_Color.png"),
-    metalnessTexture: require("./res/tesla/object_car_main_Metallic.png"),
-    roughnessTexture: require("./res/tesla/object_car_main_Roughness.png")
-  }
-});
-
 ViroARTrackingTargets.createTargets({
   logo: {
     source: require("./res/logo.png"),
     orientation: "Up",
     physicalWidth: 0.165 // real world width in meters
-  }
-});
-
-ViroAnimations.registerAnimations({
-  scaleUp: {
-    properties: { scaleX: 1, scaleY: 1, scaleZ: 1 },
-    duration: 500,
-    easing: "bounce"
-  },
-  scaleDown: {
-    properties: { scaleX: 0, scaleY: 0, scaleZ: 0 },
-    duration: 200
-  },
-  scaleCar: {
-    properties: { scaleX: 0.09, scaleY: 0.09, scaleZ: 0.09 },
-    duration: 500,
-    easing: "bounce"
   }
 });
