58bd829f0cbf17d8cceabb6abb2495e0a14f33e1
Yurick <yurick.hauschild@gmail.com>
2018-12-26 15:32:48 -0200

    ✨  Try to render object over novatics card

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import InitialARScene from "./js/HelloWorldSceneAR";
 const API_KEY = "E658630C-44FE-45B4-A235-5031C4930327";
 
 export default function Skvngr() {
-    return (
-      <ViroARSceneNavigator
-        apiKey={API_KEY}
-        initialScene={{ scene: InitialARScene }}
-      />
-    )
+  return (
+    <ViroARSceneNavigator
+      apiKey={API_KEY}
+      initialScene={{ scene: InitialARScene }}
+      autofocus
+      hdrEnabled
+    />
+  );
 }
--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,42 +1,31 @@
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import { ViroARScene, ViroText, ViroConstants } from "react-viro";
+import {
+  ViroARScene,
+  ViroARImageMarker,
+  ViroARTrackingTargets,
+  ViroBox
+} from "react-viro";
+
+ViroARTrackingTargets.createTargets({
+  targetOne: {
+    source: require("./res/card.jpg"),
+    orientation: "Up",
+    physicalWidth: 0.1 // real world width in meters
+  }
+});
 
 export default class HelloWorldSceneAR extends Component {
-  state = {
-    text: 'Initializing SKVNGR...'
-  }
+  log = () => {
+    console.log("HELLO", "💩 ".repeat(100));
+  };
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this.onInitialized}>
-        <ViroText
-          text={this.state.text}
-          scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
-          style={styles.helloWorldTextStyle}
-        />
+      <ViroARScene>
+        <ViroARImageMarker target="targetOne" onAnchorFound={this.log}>
+          <ViroBox position={[0, 0.25, 0]} scale={[0.5, 0.5, 0.5]} />
+        </ViroARImageMarker>
       </ViroARScene>
     );
   }
-
-  onInitialized = (state, reason) => {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text: "novatics!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
 }
-
-const styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: "Arial",
-    fontSize: 30,
-    color: "#ffffff",
-    textAlignVertical: "center",
-    textAlign: "center"
-  }
-});
