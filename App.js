5273cc0185e76ac509d6afd09db8072cd5b119bf
Raphael Santos <raphael.s.cunha@gmail.com>
2018-12-26 14:40:12 -0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { ViroARSceneNavigator } from "react-viro";
+
+var sharedProps = {
+  apiKey: "E658630C-44FE-45B4-A235-5031C4930327"
+};
+
+// Sets the default scene you want for AR and VR
+var InitialARScene = require("./js/HelloWorldSceneAR");
+
+var UNSET = "UNSET";
+
+// This determines which type of experience to launch in, or UNSET, if the user should
+// be presented with a choice of AR or VR. By default, we offer the user a choice.
+var defaultNavigatorType = UNSET;
+
+export default class ViroSample extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      navigatorType: defaultNavigatorType,
+      sharedProps: sharedProps
+    };
+    this._exitViro = this._exitViro.bind(this);
+  }
+
+  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
+  // if you are building a specific type of experience.
+  render() {
+    return (
+      <ViroARSceneNavigator
+        {...this.state.sharedProps}
+        initialScene={{ scene: InitialARScene }}
+      />
+    );
+  }
+
+  // This function "exits" Viro by setting the navigatorType to UNSET.
+  _exitViro() {
+    this.setState({
+      navigatorType: UNSET
+    });
+  }
+}
+
+var localStyles = StyleSheet.create({
+  viroContainer: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  outer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  inner: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  titleText: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 25
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 20
+  },
+  buttons: {
+    height: 80,
+    width: 150,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  exitButton: {
+    height: 50,
+    width: 100,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#68a0cf",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  }
+});
+
+module.exports = ViroSample;
