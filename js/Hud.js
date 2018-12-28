b132879e536bf999115384860d33d45549a275b8
Yurick <yurick.hauschild@gmail.com>
2018-12-28 16:09:19 -0200

    ♻️  Extract progress button

--- js/Hud.js
+++ js/Hud.js
@@ -1,76 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { View, Dimensions, Text } from 'react-native';
-import ProgressCircle from 'react-native-progress-circle';
+import { View, Dimensions } from 'react-native';
 
-function ProgressButton({ total, current }) {
-  const diameter = 92;
-  const innerRadius = 38;
-  const textBoxHeight = 30;
-  const stripeWidth = 5;
-
-  const baseTextStyle = {
-    color: '#fff',
-    paddingBottom: innerRadius - textBoxHeight / 2 - stripeWidth,
-  };
-
-  return (
-    <View
-      style={{
-        width: diameter,
-        height: diameter,
-        borderRadius: diameter / 2,
-        backgroundColor: '#3c3352',
-        overflow: 'hidden',
-        justifyContent: 'center',
-        alignItems: 'center',
-        // opacity: 0.5,
-      }}
-    >
-      <ProgressCircle
-        percent={(100 * current) / total}
-        radius={innerRadius}
-        borderWidth={stripeWidth}
-        fill={current}
-        color="#f4816a"
-        bgColor="#3c3352"
-        shadowColor="#3c3352"
-        containerStyle={{
-          flexDirection: 'row',
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-        }}
-      >
-        <Text
-          style={{
-            ...baseTextStyle,
-            fontSize: textBoxHeight,
-          }}
-        >
-          {current}
-        </Text>
-        <Text
-          style={{
-            ...baseTextStyle,
-            fontSize: textBoxHeight / 2,
-          }}
-        >
-          /{total}
-        </Text>
-      </ProgressCircle>
-    </View>
-  );
-}
-
-ProgressButton.propTypes = {
-  total: PropTypes.number,
-  current: PropTypes.number,
-};
-
-ProgressButton.defaultProps = {
-  total: 0,
-  current: 0,
-};
+import ProgressButton from './ProgressButton';
 
 export default function Hud() {
   return (
